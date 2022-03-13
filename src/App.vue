251271e345eb50f978534b388843f13ed1efb1d2
vitor costa <vitorjjhg@hotmail.com>
2022-03-13 14:21:34 -0300

    AppTopBar

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,49 @@
 <template>
   <v-app>
-    <v-app-bar app color="green" dark>
-      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
-      <v-toolbar-title>Campeonato Brasileiro ⚽</v-toolbar-title>
-    </v-app-bar>
-    <v-main> eae </v-main>
+    <AppTopBar />
+
+    <v-navigation-drawer v-model="drawer" absolute temporary>
+      <v-list nav dense>
+        <v-list-item-group
+          v-model="group"
+          active-class="green--text text--lighten-2"
+        >
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Inicio</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-soccer</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title> Classificação </v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-main>
+      <Inicio />
+    </v-main>
   </v-app>
 </template>
 
 <script>
+import Inicio from './components/Inicio.vue';
+import AppTopBar from './components/shared/AppTopBar.vue';
+
 export default {
   name: "App",
-
-  components: {},
-
+  components: {
+    Inicio,
+    AppTopBar,
+  },
   data: () => ({
-    //
+    drawer: false,
+    group: null,
   }),
 };
 </script>
---
+++ src/components/shared/AppTopBar.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-app-bar app color="green" dark>
+      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+      <v-toolbar-title>Campeonato Brasileiro ⚽</v-toolbar-title>
+    </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: "AppTopBar"
+}
+</script>
+
+<style>
+
+</style>
